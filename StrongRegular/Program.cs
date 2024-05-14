3cf18785ccbe6ae6202115e3a7e00645954e906b
The4yo <eugshpak@yandex.ru>
2024-05-14 21:03:47 +0300

    Добавьте файлы проекта.

---
+++ StrongRegular/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите строку:");
+string inputstring = Console.ReadLine();
+
+string needstring = @"\b(\w+)\b(?<=\b\1\b.*\b\1\b)|\b(\w+)\b(?=.*\b\2\b)";
+
+string resultstring = Regex.Replace(inputstring, needstring , @"<strong>$1$2</strong>");
+
+Console.WriteLine(resultstring);
